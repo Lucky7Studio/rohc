0ad6613dce297b22b84592336503e90a52d2d085
Didier Barvaux <didier@barvaux.org>
2017-05-27 18:00:31 +0200

    CR: be sure to reset the counters of IR-CR packets

--- src/comp/rohc_comp.c
+++ src/comp/rohc_comp.c
@@ -2329,6 +2329,7 @@ static struct rohc_comp_ctxt *
 		memcpy(c, &(comp->contexts[cid_for_replication]), sizeof(struct rohc_comp_ctxt));
 		c->do_ctxt_replication = true;
 		c->cr_base_cid = cid_for_replication;
+		c->cr_count = 0;
 	}
 	else
 	{
